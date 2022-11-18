65c9ec06e2192bb23c9007abcd9aa7f21b7b2177
Hayden Bradfield <hbradfield20@gmail.com>
2022-11-18 14:49:56 -0500

    work on threads page

--- src/views/Thread.vue
+++ src/views/Thread.vue
@@ -12,19 +12,51 @@
                 <div class="thread__content" v-html="threadData.content"></div>
             </section>
         </div>
+        <form class="opinion-form" v-if="showResponseForm" @submit.prevent>
+            <h2>Add an Opinion</h2>
+            <quill-editor 
+            theme="snow"
+            :toolbar="toolbar"
+            placeholder="Write something interesting..."
+            />
+            <button class="btn">Submit</button>
+        </form>
+        <div class="auth-prompt" v-if="showAuthPrompt">
+            <p><a @click="openAuthModal">Login or Register</a> to add your opinion.</p>
+        </div>
+        <div class="opinions">
+            <h2>Opinions (0)</h2>
+
+        </div>
     </div>
 </template>
 
 <script lang="ts" setup>
-    import { ref, reactive, onMounted } from 'vue';
+    import { ref, reactive, onMounted, computed } from 'vue';
     import axios from 'axios';
     import { useRoute, useRouter } from 'vue-router';
+    import { QuillEditor } from '@vueup/vue-quill';
+    import '@vueup/vue-quill/dist/vue-quill.snow.css';
+    import { useAuthStore } from '@/stores/useAuthStore';
+    import { useCoreModalStore } from '@/stores/useCoreModalStore';
 
     const { params: routeParams } = useRoute();
     const { push: routerPush } = useRouter();
+    const { getIsLoggedIn, getUserData } = useAuthStore();
+    const { openAuthModal } = useCoreModalStore();
 
     const slug = ref<string>(routeParams.slug);
     const threadData = reactive({});
+
+    const toolbar = ['bold', 'italic', 'underline', {'list': 'ordered'}, {'list': 'bullet'}];
+
+    const showResponseForm = computed(() => {
+        return getIsLoggedIn.value === true && threadData.author_user_id !== getUserData.user_id;
+    });
+
+    const showAuthPrompt = computed(() => {
+        return getIsLoggedIn.value === false;
+    });
 
     const fetchThread = async () => {
 
@@ -45,10 +77,9 @@
 
                 image.style.display = 'block';
                 image.style.margin = '1rem auto';
+                image.style.maxWidth = '300px';
                 image.src = `http://localhost:3002/thread_img/${url.pathname.split('/')[2]}`;
             });
-
-            console.log(dom.body.innerHTML);
 
             thread.content = dom.body.innerHTML;
 
@@ -101,7 +132,7 @@
         }
 
         &__headline{
-            font-size:1.6rem;
+            font-size:1.8rem;
             color:#555;
         }
 
@@ -112,6 +143,48 @@
 
         .user-icon{
             margin-right:.5rem;
+        }
+    }
+
+    .opinion-form, 
+    .auth-prompt {
+        background:#fff;
+        padding:2rem;
+        box-shadow:0 0 .5rem rgba(0,0,0,.24);
+        margin:2rem 0;
+        border-radius:.5rem;
+
+        h2{
+            font-size:2.2rem;
+            margin-bottom:1rem;
+        }
+    }
+
+    .btn{
+        display:inline-block;
+        background:#21bf8f;
+        border:none;
+        padding:.8rem 1.2rem;
+        color:#fff;
+        font-size:1.6rem;
+        font-weight:bold;
+        border-radius:.5rem;
+        margin:1rem 0;
+    }
+
+    .auth-prompt{
+        font-size:1.6rem;
+
+        a{
+            color:#11998e;
+        }
+    }
+
+    .opinions{
+        margin:2rem 0;
+
+        h2{
+            font-size:2.2rem;
         }
     }
 
