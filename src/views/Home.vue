be9b926b0db5d12abb6235d44e3192f71c9d91df
Hayden Bradfield <hbradfield20@gmail.com>
2022-11-18 09:48:04 -0500

    work on home/live-feed view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,64 @@
 <template>
-    <div>
-        Home
+    <div class="container">
+        <div class="threads">
+            
+        </div>
     </div>
 </template>
+
+<script lang="ts" setup>
+    import { ref, reactive, onMounted } from 'vue';
+    import axios from 'axios';
+
+    const shownThreads = reactive([]);
+    const newThreadsToLoad = reactive([]);
+
+    const maxId = ref<number | null>(null);
+
+    const loadThreads = async () => {
+
+        try{
+
+            if(maxId.value !== null && maxId.value < 1){
+                return;
+            }
+
+            const response = await axios.get(`http://localhost:3002/api/v1/thread?maxId=${maxId.value}`);
+
+            const threads = response.data.body.threads;
+
+            maxId.value = Math.min(...threads.map(thread => +thread.id)) - 1;
+
+            shownThreads.push(...threads);
+
+        }catch(e){
+            console.error(e);
+        }
+
+    }
+
+    onMounted(() => {
+        loadThreads();
+    });
+
+
+</script>
+
+<style lang="scss" scoped>
+
+    .container{
+        width:75%;
+        padding:1rem;
+        margin:2rem auto;
+    }
+
+
+    @media (max-width: 800px) {
+
+        .container{
+            width:100%;
+        }
+
+    }
+
+</style>
